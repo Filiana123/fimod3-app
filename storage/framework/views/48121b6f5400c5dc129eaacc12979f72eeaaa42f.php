da13d02125ed4bde81c3fa816289ca2bb5cc7780
Filiana123 <164728654+Filiana123@users.noreply.github.com>
2024-03-24 16:49:30 +0700

    Add files via upload

---
+++ storage/framework/views/48121b6f5400c5dc129eaacc12979f72eeaaa42f.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css ">
+    <link rel="stylesheet" href="dosenSI.css">
+
+    <title>Lecturers & Staff » Information Systems Study Program » Telkom University Surabaya </title>
+    <link rel="shortcut icon" href="assets/images/telu.png" type="image/x-icon">
+
+</head>
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+    <div class="bg-purple">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <img class="img-fluid" src="assets/images/telu logo.png" alt="logo" style="width: 120px;">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- dropdown -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                About Us
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">General Information</a></li>
+                                <li><a class="dropdown-item" href="#">Organization</a></li>
+                                <li><a class="dropdown-item" href="#">Appreciation</a></li>
+                                <li><a class="dropdown-item" href="#">Collaborate</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Academic
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Curriculum Structure</a></li>
+                                <li><a class="dropdown-item" href="#">Study Plan</a></li>
+                                <li><a class="dropdown-item" href="#">Scholarship</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Campus Life
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Life in Surabaya</a></li>
+                                <li><a class="dropdown-item" href="#">Event Gallery</a></li>
+                                <li><a class="dropdown-item" href="#">Research</a></li>
+                                <li><a class="dropdown-item" href="#">Community Service</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Lecturers & Staff</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                    </ul>
+                    <button class="btn btn-primary">Sign Up Now
+                    </button>
+                </div>
+            </div>
+        </nav>
+        <div class="purple-box">
+            <h1 class="h1">Lecturer & Staff of Information Systems</h1>
+            <p class="p">ITTelkom Surabaya Information System Lecturers & Staff are experienced, have backgrounds
+                as <br>
+                researchers, academics, and practitioners in the industry</p>
+        </div>
+        <!-- baris 1 -->
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Ully.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Ully Asfari, S.Kom., M.Kom.</h5>
+                    <p class="card-text">IT Portfolio Management, Organizational Culture, Human Computer Interaction</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Nizar.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mochamad Nizar Palefi Ma’ady, S.Kom., M.Kom., M.IM </h5>
+                    <p class="card-text">Dynamic Programming, Machine Learning, Social Computing</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Agus-Sulistya.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Agus Sulistya, P.hd</h5>
+                    <p class="card-text">Social Computing, Data Scientist & Engineering, Software Engineering, IT
+                        Planning</p>
+                </div>
+            </div>
+        </div>
+        <!-- baris 2 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/YupitNo-Photo.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Yupit Sudianto, S.Kom., M.Kom.</h5>
+                    <p class="card-text">Software Engineering, Enterprise Architecture, Internet of Things</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Hawwin.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Hawwin Mardhiana, S.Kom., M.Kom.</h5>
+                    <p class="card-text">Modeling and Simulation, DSS, Enterprise Systems</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Toton.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Noerma Pudji Istiyanto, S.Kom., M.Kom</h5>
+                    <p class="card-text">Bussiness Intelligence, Project Management, e-Government, e-SME, Start-Up</p>
+                </div>
+            </div>
+        </div>
+        <!-- baris 3 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Iin.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Rokhmatul Insani, S.T., M.T.</h5>
+                    <p class="card-text">Data Warehouse, Data Base, Data Mining</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Nasrul.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Muhamad Nasrullah, S.Kom., M.Kom.</h5>
+                    <p class="card-text">Smart City, Adopsi IT, Security Engineering</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/YupitNo-Photo.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aris Kusumawati, S.Kom., M.Kom</h5>
+                    <p class="card-text">IT Business Value, Strategic Information System, IT Risk Management, IT
+                        Governance, Management Information System</p>
+                </div>
+            </div>
+        </div>
+        <!-- baris 4 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/YupitNo-Photo.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Purnama Anaking, S.Kom, M.Kom</h5>
+                    <p class="card-text">Software Engineering, Hybrid Mobile App Development, Agile Project Management,
+                        Human Computer Interaction</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/aan.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Anfazul Faridatul Azizah, S.Kom., M.Kom. </h5>
+                    <p class="card-text">Interaction design, Tata kelola IT, IT Quality Management, IT Project
+                        Management, Adoption Technology, IT Audit</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Fian.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Alifiansyah Arrizqy Hidayat, S.Kom., M.Kom.</h5>
+                    <p class="card-text">Customer Relationship Management, Adoption and Acceptance of Information
+                        Technology, Business Process Management</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- baris 5 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/YupitNo-Photo.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Adzanil Rachmadhi Putra, S.Kom, M.Kom</h5>
+                    <p class="card-text">Geo Information System, Web Development, Software Engineering</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Arip.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Arip Ramadan, S.Si., M.Stat.</h5>
+                    <p class="card-text">Computational Statistics, Linear Model, Nonparametric Regression</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Berlian.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Berlian Rahmy Lidiawaty, S.ST., M.MT.</h5>
+                    <p class="card-text">Text Mining, Risk Management, Data Management</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- baris 6 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Anita.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Anita Hakim Nasution, S.T., M.T.</h5>
+                    <p class="card-text">Risk Management, Human Resource Development, Sales Management, Supply Chain
+                        Management, Bussiness Development</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Fina.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Affifiana Prisyanti, S.Kom, M.Kom</h5>
+                    <p class="card-text">Sistem Enterprise, Information System strategy management</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Anita.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Anisa Dzulkarnain, S.Kom., M.Kom</h5>
+                    <p class="card-text">Dynamic System Modeling, System Thinking, Travelling Salesman Problem,
+                        Geographic Information Systems</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- baris 7 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Rosyid.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Rosyid Abdillah, S.Si., M.Kom</h5>
+                    <p class="card-text">UI Design, User Experience & Data Analysis</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Raulia.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Raulia Riski, S.Kom., M.Kom., M.M.Sc.</h5>
+                    <p class="card-text">Enterprise Systems, Modeling and Simulation, Business Process Management</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Tita.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tita Ayu Rospricilia, S.Kom., M.Kom.</h5>
+                    <p class="card-text">Customer Relationship Management, Information System Management, Enterprise
+                        System</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- baris 8 -->
+
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/YupitNo-Photo.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Sri Hidayati, S.Si., M.Stat.</h5>
+                    <p class="card-text">Applied Statistics, Data Analytics, Time Series Forecasting</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/images/Pak-Ilham.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Muhammad Ilham Alhari, S.Kom., M.Kom.</h5>
+                    <p class="card-text">IT Governance, Enterprise Architecture, Smart Village</p>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-column1">
+                <h5>Information System</h5>
+                <p>Official website of Telkom University Information Systems Study Program, Surabaya. <br> This website
+                    is intended as a medium of internal & external communication for students, lecturers, and employees.
+                    <br>
+                    <br>
+                    The Information Systems Study Program has existed since the establishment of Telkom University
+                    Surabaya, based on the Decree of the Minister of Research, Technology and Higher Education no. 733 /
+                    KPT / I / 2018.
+                    <br>
+                    <br>
+                    This website presents information about the Information Systems Study Program, profile, vision and
+                    mission, organizational structure, academics, curriculum, lecturers, research and community service.
+                </p>
+            </div>
+            <div class="footer-column2">
+                <h5>Important Links</h5>
+                <br>
+                <p class="hover-text">Collaborate</p>
+                <p class="hover-text">Blog</p>
+                <p class="hover-text">Appreciation</p>
+                <p class="hover-text">Scholarship</p>
+                <br>
+                <bt>
+                    <br>
+                    <h5>Helpful Links</h5>
+                    <br>
+                    <p class="hover-text">Telkom Indonesia</p>
+                    <p class="hover-text">Telkom Education Foundation</p>
+                    <p class="hover-text">Telkom University Surabaya</p>
+                    <p class="hover-text">Faculty (FTIB)</p>
+                    <p class="hover-text">Website SMB</p>
+                </bt>
+            </div>
+            <div class="footer-column3">
+                <footer>
+                    <div class="footer-column3">
+                        <h5>Contact Info</h5>
+                        <div class="hover-icon">
+                            <i class="bi bi-geo" style="color: white"></i>
+                            <div class="info">
+                                <span class="info-text">Address: </span>
+                                <br>
+                                <span class="hover-text">Jl. Ketintang No.156, Surabaya, East Java 60231</span>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="hover-icon">
+                            <i class="bi bi-telephone"></i>
+                            <div class="info">
+                                <span class="info-text">Telephone: </span>
+                                <br>
+                                <span class="hover-text">+62 31 8280800</span>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="hover-icon">
+                            <i class="bi bi-envelope"></i> <!-- Icon email dari Bootstrap Icons -->
+                            <div class="info">
+                                <span class="info-text">Email: </span>
+                                <br>
+                                <span class="hover-text">is@ittelkom-sby.ac.id</span>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+
+</body>
+
+</html>
+<?php /**PATH C:\MATKUL SEMESTER 4\Pemrograman Framework\fimod3-app\resources\views/DosenSI.blade.php ENDPATH**/ ?>
